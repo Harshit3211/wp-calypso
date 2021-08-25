Harshit3211/wp-calypso

commit 1db6c5dee91063727a9aaad3fe14f8066b9993a2

Adding account-only signup flow (#54715)
* Adding account-only signup flowZ

* Updating redirect state for WPcom only account creation

* Passing redirect info through the /start/account flow

* Adding redirect processing to User flow

* Cleaning up superfluous comment

* Swapping indexOf to includes

* Addressing review feedback

client/lib/login/index.js
@@ -121,5 +121,12 @@ export function getSignupUrl(
 		signupUrl = `${ signupUrl }/wpcc?${ oauth2Params.toString() }`;
 	}
 
+	if ( includes( redirectTo, 'action=jetpack-sso' ) && includes( redirectTo, 'sso_nonce=' ) ) {
+		const params = new URLSearchParams( {
+			redirect_to: redirectTo,
+		} );
+		signupUrl = `/start/account?${ params.toString() }`;
+	}
+
 	return signupUrl;
 }

client/lib/paths/login/index.js
@@ -49,7 +49,9 @@ export function login( {
 	}
 
 	if ( redirectTo ) {
-		url = addQueryArgs( { redirect_to: redirectTo }, url );
+		url = redirectTo.includes( 'jetpack-sso' )
+			? redirectTo
+			: addQueryArgs( { redirect_to: redirectTo }, url );
 	}
 
 	if ( emailAddress ) {

client/signup/config/flows-pure.js
@@ -16,7 +16,7 @@ export function generateFlows( {
 		{
 			name: 'account',
 			steps: [ 'user' ],
-			destination: '/',
+			destination: getRedirectDestination,
 			description: 'Create an account without a blog.',
 			lastModified: '2020-08-12',
 			pageTitle: translate( 'Create an account' ),

client/signup/config/flows.js
@@ -46,6 +46,8 @@ function getRedirectDestination( dependencies ) {
 			new URL( dependencies.oauth2_redirect ).host === 'public-api.wordpress.com'
 		) {
 			return dependencies.oauth2_redirect;
+		} else if ( dependencies.redirect ) {
+			return dependencies.redirect;
 		}
 	} catch {
 		return '/';

client/signup/config/steps-pure.js
@@ -138,6 +138,7 @@ export function generateSteps( {
 				'username',
 				'marketing_price_group',
 				'plans_reorder_abtest_variation',
+				'redirect',
 			],
 			optionalDependencies: [ 'plans_reorder_abtest_variation' ],
 			props: {

client/signup/steps/user/index.jsx
@@ -224,6 +224,8 @@ export class UserStep extends Component {
 		if ( oauth2Signup ) {
 			dependencies.oauth2_client_id = data.queryArgs.oauth2_client_id;
 			dependencies.oauth2_redirect = data.queryArgs.oauth2_redirect;
+		} else if ( data.queryArgs.redirect_to ) {
+			dependencies.redirect = data.queryArgs.redirect_to;
 		}
 
 		this.props.submitSignupStep(
@@ -381,6 +383,9 @@ export class UserStep extends Component {
 			this.isOauth2RedirectValid( this.props.initialContext.query.oauth2_redirect )
 		) {
 			return this.props.initialContext.query.oauth2_redirect;
+		}
+		if ( this.props.initialContext?.canonicalPath?.startsWith( '/start/account' ) ) {
+			return this.props.initialContext.query.redirect_to;
 		}
 
 		const stepAfterRedirect =

client/state/login/reducer.js
@@ -84,6 +84,8 @@ export const redirectTo = combineReducers( {
 				const { path, query } = action;
 				if ( startsWith( path, '/log-in' ) ) {
 					return query.redirect_to || state;
+				} else if ( startsWith( path, '/start/account' ) ) {
+					return query.redirect_to || state;
 				} else if ( '/jetpack/connect/authorize' === path ) {
 					return addQueryArgs( query, path );
 				}

commit 846b7578c51b69dc78f24d576c2d45f3c4392fed

This PR adds an account-only signup flow and fixes the regression introduced in #54715 by moving redirect to optional dependency

client/lib/login/index.js
@@ -128,5 +128,12 @@ export function getSignupUrl(
 		signupUrl = `${ signupUrl }/wpcc?${ oauth2Params.toString() }`;
 	}
 
+	if ( includes( redirectTo, 'action=jetpack-sso' ) && includes( redirectTo, 'sso_nonce=' ) ) {
+		const params = new URLSearchParams( {
+			redirect_to: redirectTo,
+		} );
+		signupUrl = `/start/account?${ params.toString() }`;
+	}
+
 	return signupUrl;
 }

client/lib/paths/login/index.js
@@ -48,7 +48,9 @@ export function login( {
 	}
 
 	if ( redirectTo ) {
-		url = addQueryArgs( { redirect_to: redirectTo }, url );
+		url = redirectTo.includes( 'jetpack-sso' )
+			? redirectTo
+			: addQueryArgs( { redirect_to: redirectTo }, url );
 	}
 
 	if ( emailAddress ) {

client/signup/config/flows-pure.js
@@ -16,7 +16,7 @@ export function generateFlows( {
 		{
 			name: 'account',
 			steps: [ 'user' ],
-			destination: '/',
+			destination: getRedirectDestination,
 			description: 'Create an account without a blog.',
 			lastModified: '2020-08-12',
 			pageTitle: translate( 'Create an account' ),

client/signup/config/flows.js
@@ -46,6 +46,8 @@ function getRedirectDestination( dependencies ) {
 			new URL( dependencies.oauth2_redirect ).host === 'public-api.wordpress.com'
 		) {
 			return dependencies.oauth2_redirect;
+		} else if ( dependencies.redirect ) {
+			return dependencies.redirect;
 		}
 	} catch {
 		return '/';

client/signup/config/steps-pure.js
@@ -139,7 +139,7 @@ export function generateSteps( {
 				'marketing_price_group',
 				'plans_reorder_abtest_variation',
 			],
-			optionalDependencies: [ 'plans_reorder_abtest_variation' ],
+			optionalDependencies: [ 'plans_reorder_abtest_variation', 'redirect' ],
 			props: {
 				isSocialSignupEnabled: config.isEnabled( 'signup/social' ),
 			},

client/signup/steps/user/index.jsx
@@ -255,6 +255,8 @@ export class UserStep extends Component {
 		if ( oauth2Signup ) {
 			dependencies.oauth2_client_id = data.queryArgs.oauth2_client_id;
 			dependencies.oauth2_redirect = data.queryArgs.oauth2_redirect;
+		} else if ( data.queryArgs.redirect_to ) {
+			dependencies.redirect = data.queryArgs.redirect_to;
 		}
 
 		this.props.submitSignupStep(
@@ -402,6 +404,32 @@ export class UserStep extends Component {
 		}
 
 		return headerText;
+	}
+
+	getRedirectToAfterLoginUrl() {
+		if (
+			this.props.oauth2Signup &&
+			this.props.initialContext &&
+			this.props.initialContext.query.oauth2_redirect &&
+			this.isOauth2RedirectValid( this.props.initialContext.query.oauth2_redirect )
+		) {
+			return this.props.initialContext.query.oauth2_redirect;
+		}
+		if ( this.props.initialContext?.canonicalPath?.startsWith( '/start/account' ) ) {
+			return this.props.initialContext.query.redirect_to;
+		}
+
+		const stepAfterRedirect =
+			getNextStepName( this.props.flowName, this.props.stepName, this.props.userLoggedIn ) ||
+			getPreviousStepName( this.props.flowName, this.props.stepName, this.props.userLoggedIn );
+		const queryArgs = new URLSearchParams( this.props?.initialContext?.query );
+		const queryArgsString = queryArgs.toString() ? '?' + queryArgs.toString() : '';
+
+		return (
+			window.location.origin +
+			getStepUrl( this.props.flowName, stepAfterRedirect ) +
+			queryArgsString
+		);
 	}
 
 	submitButtonText() {

client/state/login/reducer.js
@@ -77,6 +77,8 @@ export const redirectTo = combineReducers( {
 				const { path, query } = action;
 				if ( startsWith( path, '/log-in' ) ) {
 					return query.redirect_to || state;
+				} else if ( startsWith( path, '/start/account' ) ) {
+					return query.redirect_to || state;
 				} else if ( '/jetpack/connect/authorize' === path ) {
 					return addQueryArgs( query, path );
 				}